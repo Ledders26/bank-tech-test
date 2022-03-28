7b6ba14e42245d7736abe6ebbb7f551bdbbef80e
Ledders26 <benlhall584@hotmail.co.uk>
2022-03-28 15:02:10 +0100

    added deposit test and code

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,8 +1,16 @@
 const Account = require('../lib/account')
 
+beforeEach(() => {
+  account = new Account();
+});
+
 describe("Account", () => {
   it("should have a starting balance of 0", () => {
-    account = new Account();
     expect(account.getBalance()).toEqual(0)
+  });
+
+  it("should allow money to be deposited into the account and update the balance", () => {
+    account.deposit(30)
+    expect(account.getBalance()).toEqual(30)
   });
 });
