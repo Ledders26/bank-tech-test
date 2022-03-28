8cacf341de9c9554b877f52b8ce8a44abec065c9
Ledders26 <benlhall584@hotmail.co.uk>
2022-03-28 15:05:45 +0100

    added withdraw functionality and tests

--- spec/account.test.js
+++ spec/account.test.js
@@ -4,13 +4,22 @@ beforeEach(() => {
   account = new Account();
 });
 
-describe("Account", () => {
+describe("getBalance", () => {
   it("should have a starting balance of 0", () => {
     expect(account.getBalance()).toEqual(0)
   });
+});
 
+describe("deposit", () => {
   it("should allow money to be deposited into the account and update the balance", () => {
     account.deposit(30)
     expect(account.getBalance()).toEqual(30)
+  });
+});
+
+describe("withdraw", () => {
+  it("should allow money to be withdrawn from the account and the balance updated", () => {
+    account.withdraw(30)
+    expect(account.getBalance()).toEqual(-30)
   });
 });
