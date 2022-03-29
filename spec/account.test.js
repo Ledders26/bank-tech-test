ed329fb6f5351ac3f34e73ce33db721b48bdef26
Ledders26 <benlhall584@hotmail.co.uk>
2022-03-29 11:06:17 +0100

    added requestStatement to account which creates new Statement, after writing test

--- spec/account.test.js
+++ spec/account.test.js
@@ -12,14 +12,14 @@ describe("getBalance", () => {
 
 describe("deposit", () => {
   it("should allow money to be deposited into the account and update the balance", () => {
-    account.deposit(30)
+    account.deposit(30, "28-03-2022")
     expect(account.getBalance()).toEqual(30)
   });
 });
 
 describe("withdraw", () => {
   it("should allow money to be withdrawn from the account and the balance updated", () => {
-    account.withdraw(30)
+    account.withdraw(30, "28-03-2022")
     expect(account.getBalance()).toEqual(-30)
   });
 });
@@ -29,4 +29,10 @@ describe("getTransactions", () => {
     account.deposit(100)
     expect(account.getTransactions().length).toEqual(1)
   });
-})
+});
+
+describe("requestStatement", () => {
+  it("should create a new Statement when requested", () => {
+  expect(typeof (account.requestStatement())).toBe('object')
+  });
+});
--- spec/statement.test.js
+++ spec/statement.test.js
@@ -1,12 +1,18 @@
 const Statement = require('../lib/statement')
+const Account = require('../lib/account')
 
 beforeEach(() => {
+  account = new Account();
   statement = new Statement();
 });
 
 describe("printStatement", () => {
-  it("should return an empty statement when no recorded transactions", () => {
-    statement.createStatement()
+  it("should return headers only when no recorded transactions", () => {
     expect(statement.printStatement()).toEqual("date || credit || debit || balance")
+  });
+
+  it("should return a statement when transactions recorded", () => {
+    account.deposit(1000.00, "10-01-2023")
+    expect(statement.printStatement()).toEqual("date || credit || debit || balance\r\n10-01-2023 || 1000.00 || || 1000.00")
   });
 });
